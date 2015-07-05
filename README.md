bc81b83339256eb38330672b03d37d39835eb8bd
Марко Кажић <marko_kazic@live.com>
2015-07-05 13:08:17 +0200

    Initial bot import

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # lalabot
 Lalabot is a welcoming Slackbot written in Meteor
+
+## Usage
+
+Edit `settings.json` with data about your bot.
+Run with `meteor --settings settings.json`
+
+The bot is now running.
---
+++ lalabot.js
@@ -0,0 +1,31 @@
+if (Meteor.isClient) {
+
+}
+if (Meteor.isServer) {
+    Meteor.startup(function() {
+        var SlackBot = Meteor.npmRequire('slackbots');
+        var bot = new SlackBot({
+            token: Meteor.settings.slackbotToken, // Add a bot https://my.slack.com/services/new/bot and put the token
+            name: Meteor.settings.name
+        });
+        bot.on('team_join', function() {
+            // more information about additional params https://api.slack.com/methods/chat.postMessage
+            var params = {
+                icon_emoji: ':balloon:'
+            };
+
+            bot.postMessageToChannel('general', _.sample(Meteor.settings.welcomeMessage), params);
+            // bot.postMessageToUser('username', 'meow!', params);
+            // bot.postMessageToGroup('private_group', 'meow!', params);
+        });
+        bot.on('start', function() {
+            var params = {
+                icon_emoji: ':barber:'
+            };
+
+            // Notify the closest relative on bot starts
+            bot.postMessageToUser(Meteor.settings.closestRelative, Meteor.settings.startMessage + (new Date()), params);
+
+        });
+    });
+}
